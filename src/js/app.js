97df29ce12f08501caab05060cd0bb999e7ba896
Aleksei Ogoltcov <aaogoltcov@mail.ru>
2020-11-09 07:49:10 +0300

    init commit

---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+'use strict'
+
+import { Team } from './set'
+import { Character } from './hero'
+import { Bowerman } from './hero'
+import { Swordsman } from './hero'
+import { Magician } from './hero'
+import { Daemon } from './hero'
+import { Undead } from './hero'
+
+let team = new Team()
+// team.add(new Bowerman('Nick'))
+// team.add(new Bowerman('Nick'))
+// team.add(new Swordsman('Nick'))
+
+team.adAll(new Bowerman('Nick'), new Swordsman('Nick'))
+
+console.log(team.toArray())
+
+
+
